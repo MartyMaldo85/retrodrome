5a04e75d09b4c049c4cf061262546c491682efac
Martin Maldonado <martin.maldonado7@gmail.com>
2015-09-28 23:18:33 -0700

    First Commit
    First Commit to GitHub for future Retrodrome dev.

---
+++ assets/js/functions.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  $('.game-entry').on('click', function() {
+    $(this).addClass('active');
+    // $('#game-matrix').addClass('blur');
+    $('body').addClass('fixed');
+    $('console-icon')
+  });
+
+});
