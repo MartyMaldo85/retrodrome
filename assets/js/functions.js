e61e45493476c941db7e03034eaf1cbaebef615f
Martin Maldonado <martin.maldonado7@gmail.com>
2015-09-30 21:16:50 -0700

    Additional Dropdown Styling
    Further styled drop down menu and created new NES cartridge icon. Added placeholder markup for future additional icons for dropdown subsections.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,9 +2,6 @@ $(document).ready(function() {
 
   $('.game-entry').on('click', function() {
     $(this).addClass('active');
-    // $('#game-matrix').addClass('blur');
     $('body').addClass('fixed');
-    $('console-icon')
   });
-
 });
